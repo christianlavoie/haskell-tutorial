0292d1101c76856b1f1074f538397df6cb1f2212
Christian Lavoie <christian.lavoie@gmail.com>
2014-11-04 23:35:29 -0500

    More simplifications.

--- Main.hs
+++ Main.hs
@@ -1,10 +1,11 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Werror #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import Control.Concurrent (threadDelay)
-import Control.Monad (forM)
-import Data.Maybe (fromJust)
+import Control.Monad (forM_)
 import System.Process (system)
 import Text.Printf (printf)
 
@@ -14,36 +15,36 @@ import qualified Data.Map
 data Value = On | Off
   deriving (Eq, Show)
 
-data Board = Board Int Int (Data.Map.Map (Int, Int) Value)
+type CellMap = Data.Map.Map (Int, Int) Value
+
+data Board = Board Int Int CellMap
   deriving (Eq, Show)
 
 
 printBoard :: Board -> IO ()
 printBoard (Board x y cells) = do
-    forM [1 .. x] $ \x -> do
-        forM [1 .. y] $ \y -> do
-            case (cells Data.Map.! (x, y)) of
-                On -> putStr "O"
-                Off -> putStr "."
+    forM_ [1 .. x] $ \x_ -> do
+        forM_ [1 .. y] $ \y_ -> do
+            case (Data.Map.lookup (x_, y_) cells) of
+                Just On -> putStr "O"
+                Just Off -> putStr "."
+                Nothing -> error $ printf "Could not lookup index (%d, %d)" x_ y_
 
         putStr "\n"
-    return ()
 
 
 readBoard :: IO Board
 readBoard = do
     content <- getContents
     let header = Data.List.takeWhile (/= '\n') content
-    let (x, y) :: (Int, Int) = read $ head $ lines content
+    let (x, y) :: (Int, Int) = read header
 
     let footer = Data.List.dropWhile (/= '\n') content
     let filtered = filter (\e -> elem e ['.', 'O']) footer
     let values = map convert filtered
 
     -- As a hack, we use the size of the first line to determine the size of the square
-    let indexes = [ (x_, y_)
-                  | x_ <- [1 .. x]
-                  , y_ <- [1 .. y] ]
+    let indexes = [ (x_, y_) | x_ <- [1 .. x] , y_ <- [1 .. y] ]
 
     return $ Board x y (Data.Map.fromList $ zip indexes values)
 
@@ -57,7 +58,7 @@ rules = [ (On, 2), (On, 3), (Off, 3) ]
 
 
 allNeighbors :: (Int, Int) -> Board -> [Value]
-allNeighbors (x, y) (Board maxX maxY cells) =
+allNeighbors (x, y) (Board _ _ cells) =
     [ value
     | x_ <- [x - 1, x, x + 1]
     , y_ <- [y - 1, y, y + 1]
@@ -71,10 +72,9 @@ allNeighbors (x, y) (Board maxX maxY cells) =
 nextBoard :: Board -> Board
 nextBoard oldBoard@(Board oldX oldY oldCells) = Board oldX oldY newCells
   where newCells = Data.Map.mapWithKey computeCell oldCells
-        computeCell (x, y) value =
-            if elem (value, onNeighbors (x, y)) rules
-            then On
-            else Off
+
+        computeCell (x, y) value | elem (value, onNeighbors (x, y)) rules = On
+        computeCell _ _ = Off
 
         onNeighbors (x, y) = length $ filter (== On) (allNeighbors (x, y) oldBoard)
 
@@ -86,7 +86,7 @@ main = do board <- readBoard
   where go count board = do putStrLn $ printf "Iteration #%d" count
                             printBoard board
 
-                            threadDelay (100 * 1000 :: Int)
-                            system "clear"
+                            threadDelay (500 * 1000 :: Int)
+                            _ <- system "clear"
 
                             go (count + 1) (nextBoard board)
