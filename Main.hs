5c75cefb985efb9e76ad109dd0f07eb8ab5d5ef2
Christian Lavoie <christian.lavoie@gmail.com>
2014-11-04 23:16:26 -0500

    Significantly simplify codebase.

--- Main.hs
+++ Main.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Control.Concurrent (threadDelay)
 import Control.Monad (forM)
+import Data.Maybe (fromJust)
+import System.Process (system)
 import Text.Printf (printf)
 
+import qualified Data.List
 import qualified Data.Map
 
 data Value = On | Off
@@ -28,63 +33,60 @@ printBoard (Board x y cells) = do
 readBoard :: IO Board
 readBoard = do
     content <- getContents
-    let filtered = filter (\e -> elem e ['.', 'O']) content
-    let values = map convertCharToValue filtered
-    
+    let header = Data.List.takeWhile (/= '\n') content
+    let (x, y) :: (Int, Int) = read $ head $ lines content
+
+    let footer = Data.List.dropWhile (/= '\n') content
+    let filtered = filter (\e -> elem e ['.', 'O']) footer
+    let values = map convert filtered
+
     -- As a hack, we use the size of the first line to determine the size of the square
-    let size = length $ head $ lines content
+    let indexes = [ (x_, y_)
+                  | x_ <- [1 .. x]
+                  , y_ <- [1 .. y] ]
 
-    let indexes = [ (x, y) | x <- [1 .. size] , y <- [1 .. size] ]
+    return $ Board x y (Data.Map.fromList $ zip indexes values)
 
-    return $ Board size size (Data.Map.fromList $ zip indexes values)
-  where convertCharToValue 'O' = On
-        convertCharToValue '.' = Off
-        convertCharToValue other = error $ printf "Unexpected char: '%c'" other
-
-neighbors :: (Int, Int) -> Board -> [Value]
-neighbors (x, y) (Board maxX maxY cells) = [ cells Data.Map.! (x + dx, y + dy)
-                                           | dx <- [1, 0, -1]
-                                           , dy <- [1, 0, -1]
-                                           , (dx, dy) /= (0, 0)
-                                           , newX <- [x + dx]
-                                           , newY <- [y + dy]
-                                           , newX > 0 , newY > 0
-                                           , newX <= maxX , newY <= maxY ]
+  where convert 'O' = On
+        convert '.' = Off
+        convert other = error $ printf "Unexpected char: '%c'" other
 
 
-onNeighbors :: (Int, Int) -> Board -> Int
-onNeighbors (x, y) board = length ons
-  where ons = filter (== On) (neighbors (x, y) board)
+rules :: [(Value, Int)]
+rules = [ (On, 2), (On, 3), (Off, 3) ]
+
+
+allNeighbors :: (Int, Int) -> Board -> [Value]
+allNeighbors (x, y) (Board maxX maxY cells) =
+    [ value
+    | x_ <- [x - 1, x, x + 1]
+    , y_ <- [y - 1, y, y + 1]
+
+    , not $ and [x == x_, y == y_]
+    , Data.Map.member (x_, y_) cells
+
+    , value <- [cells Data.Map.! (x_, y_)] ]
+
 
 nextBoard :: Board -> Board
 nextBoard oldBoard@(Board oldX oldY oldCells) = Board oldX oldY newCells
-  where newCells = Data.Map.mapWithKey convertCell oldCells
+  where newCells = Data.Map.mapWithKey computeCell oldCells
+        computeCell (x, y) value =
+            if elem (value, onNeighbors (x, y)) rules
+            then On
+            else Off
 
-        convertCell (x, y) On = case onNeighbors (x, y) oldBoard of
-            2 -> On
-            3 -> On
-            _ -> Off
+        onNeighbors (x, y) = length $ filter (== On) (allNeighbors (x, y) oldBoard)
 
-        convertCell (x, y) Off = case onNeighbors (x, y) oldBoard of
-            3 -> On
-            _ -> Off
-
-
-countStart :: Integer
-countStart = 0
-
-countDelay :: Int
-countDelay = 100 * 1000
 
 main :: IO ()
 main = do board <- readBoard
-          putStrLn "Starting with:"
-          printBoard board
-          threadDelay countDelay
+          go (0 :: Integer) board
 
-          go countStart board
-  where go count board = do let newBoard = nextBoard board
-                            putStrLn $ printf "Iteration #%d" count
-                            printBoard newBoard
-                            threadDelay countDelay
-                            go (count + 1) newBoard
+  where go count board = do putStrLn $ printf "Iteration #%d" count
+                            printBoard board
+
+                            threadDelay (100 * 1000 :: Int)
+                            system "clear"
+
+                            go (count + 1) (nextBoard board)
