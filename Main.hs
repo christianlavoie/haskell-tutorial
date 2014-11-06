286eb9d4d513346f65fc572d9cae3a6af30c5ecf
Christian Lavoie <christian.lavoie@gmail.com>
2014-11-06 08:33:26 -0500

    Remove an out of date comment. Simplify some more.

--- Main.hs
+++ Main.hs
@@ -43,7 +43,6 @@ readBoard = do
     let filtered = filter (\e -> elem e ['.', 'O']) footer
     let values = map convert filtered
 
-    -- As a hack, we use the size of the first line to determine the size of the square
     let indexes = [ (x_, y_) | x_ <- [1 .. x] , y_ <- [1 .. y] ]
 
     return $ Board x y (Data.Map.fromList $ zip indexes values)
@@ -63,20 +62,20 @@ allNeighbors (x, y) (Board _ _ cells) =
     | x_ <- [x - 1, x, x + 1]
     , y_ <- [y - 1, y, y + 1]
 
-    , not $ and [x == x_, y == y_]
+    , not (x == x_ && y == y_)
     , Data.Map.member (x_, y_) cells
 
     , value <- [cells Data.Map.! (x_, y_)] ]
 
 
-nextBoard :: Board -> Board
-nextBoard oldBoard@(Board oldX oldY oldCells) = Board oldX oldY newCells
+iterateBoard :: Board -> Board
+iterateBoard oldBoard@(Board x y oldCells) = Board x y newCells
   where newCells = Data.Map.mapWithKey computeCell oldCells
 
-        computeCell (x, y) value | elem (value, onNeighbors (x, y)) rules = On
+        computeCell (x_, y_) value | elem (value, onNeighbors (x_, y_)) rules = On
         computeCell _ _ = Off
 
-        onNeighbors (x, y) = length $ filter (== On) (allNeighbors (x, y) oldBoard)
+        onNeighbors (x_, y_) = length $ filter (== On) (allNeighbors (x_, y_) oldBoard)
 
 
 main :: IO ()
@@ -89,4 +88,4 @@ main = do board <- readBoard
                             threadDelay (500 * 1000 :: Int)
                             _ <- system "clear"
 
-                            go (count + 1) (nextBoard board)
+                            go (count + 1) (iterateBoard board)
